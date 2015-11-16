3fe225e1fa7b35afacc9401e5d228cab62cb2609
Phuong <phuongnhu0894@gmail.com>
2015-11-16 22:44:07 +0700

    Second Commit

--- SkewImage/ViewController.m
+++ SkewImage/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
 
 @end
 
@@ -16,26 +18,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.slider addTarget:self action:@selector(changeAngle) forControlEvents:UIControlEventValueChanged];
     
-    
-    UIImage*    flag = [UIImage imageNamed:@"france.png"];
-    CALayer*    myLayer = [CALayer layer];
-    CGFloat nativeWidth = CGImageGetWidth(flag.CGImage);
-    CGFloat nativeHeight = CGImageGetHeight(flag.CGImage);
-    CGRect startFrame = CGRectMake((self.view.bounds.size.width - nativeWidth)/2, 100,nativeWidth, nativeHeight);
-    myLayer.contents = (id)flag.CGImage;
-    myLayer.frame = startFrame;
-    [self.view.layer addSublayer:myLayer];
-    myLayer.anchorPoint = CGPointMake(0.5, 0.5);
-    CATransform3D t = CATransform3DIdentity;
-    t.m34 = 1.0/ -500;
-    t = CATransform3DRotate(t, 45.0f * M_PI / 180.0f, 0, 1, 0);
-    myLayer.transform = t;
+//    UIImage* flag = [UIImage imageNamed:@"france.png"];
+//    CALayer* myLayer = [CALayer layer];
+//    CGFloat nativeWidth = CGImageGetWidth(flag.CGImage);
+//    CGFloat nativeHeight = CGImageGetHeight(flag.CGImage);
+//    CGRect startFrame = CGRectMake((self.view.bounds.size.width - nativeWidth)/2, 100,nativeWidth, nativeHeight);
+//    myLayer.contents = (id)flag.CGImage;
+//    myLayer.frame = startFrame;
+//    myLayer.anchorPoint = CGPointMake(0.5, 0.5);
+//    [self.view.layer addSublayer:myLayer];
+}
+//- (IBAction)changeAngle:(id)sender {
+//    CGFloat angle = 180.0f * ((UISlider *)sender).value * M_PI / 180.0f;
+//    CATransform3D t = CATransform3DIdentity;
+//    t.m34 = 1.0/ -500;
+//    t = CATransform3DRotate(t, angle, 0, 1, 0);
+//    
+//    CALayer *myLayer =  self.imageView.layer;
+//    myLayer.transform = t;
+//}
 
+- (void)changeAngle{
+    CGFloat angle = 180.0f * self.slider.value * M_PI / 180.0f;
+        CATransform3D t = CATransform3DIdentity;
+        t.m34 = 1.0/ -500;
+        t = CATransform3DRotate(t, angle, 0, 1, 0);
     
+        CALayer *myLayer =  self.imageView.layer;
+        myLayer.transform = t;
 
 }
-
 
 
 @end
